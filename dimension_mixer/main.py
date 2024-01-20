49565da9d3aa5b9f2d15d87077172d04c1a17feb
evelynmitchell <efmphone@gmail.com>
2024-01-19 19:44:19 -0700

    refactor main

--- dimension_mixer/main.py
+++ dimension_mixer/main.py
@@ -1,72 +1,7 @@
-# DimensionMixerBlock
+"""Dimension Mixer Block for Attention and MLP with permutation"""
 
 import torch
 from torch import nn, Tensor
-
-#from zeta.nn import (
-#    img_to_text,
-#    video_to_text,
-#    audio_to_text,
-#    Attention,
-#    FeedForward,
-#)
-#from local_attention import LocalAttention
-
-class DimensionMixerBlock(nn.Module):
-    def __init__(
-        self,
-        # dim: int,
-        # heads: int = 8,
-        # dim_head: int = 64,
-        # dropout: float = 0.1,
-        # window_size: int = 512,
-        # causal: bool = True,
-        # look_backward: int = 1,
-        # look_forward: int = 0,
-        # seqlen: int = 1028,
-        # ff_mult: int = 4,
-        *args,
-        **kwargs,
-    ):
-        super().__init__()
-            self.dim = dim
-            self.heads = heads
-            self.dim_head = dim_head
-            self.dropout = nn.Dropout(dropout)
-            self.window_size = window_size
-            self.seqlen = seqlen
-            self.ff_mult = ff_mult
-
-            inner_dim = dim_head * heads
-
-            self.local_attn = LocalAttention(
-                window_size,
-                causal,
-                look_backward,
-                look_forward,
-                dropout,
-                dim=dim,
-                autopad=True,
-                *args,
-                **kwargs,
-            )
-        
-        self.attn = Attention(
-            dim,
-            dim_head,
-            heads,
-            causal,
-            flash=False,
-            dropout=dropout,
-            qk_norm=True,   
-        )
-        
-        self.ffn = FeedForward(
-            dim,
-            dim,
-            ff_mult,
-            post_act_ln=True
-        )
 
 class ButterflyMLP(nn.Module):
         def __init__(
